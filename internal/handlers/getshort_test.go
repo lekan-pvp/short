9cd3e44236a5204e329474ab45d7e7d2cc88b7a3
lekan <lekan.lekan@gmail.com>
2022-03-01 21:19:02 +0300

    new v.1.2

---
+++ internal/handlers/getshort_test.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetShort(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetShort(tt.args.w, tt.args.r)
+		})
+	}
+}
