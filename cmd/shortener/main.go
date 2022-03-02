32be62067e1ef400f2afa30532974a37d3f489fe
lekan <lekan.lekan@gmail.com>
2022-03-02 20:04:22 +0300

    new v.3.8

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lekan-pvp/short/internal/handlers"
 	"github.com/lekan-pvp/short/internal/memrepo"
 	"github.com/lekan-pvp/short/internal/mware"
+
 	"log"
 	"net/http"
 )
@@ -18,25 +19,25 @@ func main() {
 	serverAddress := config.GetServerAddress()
 
 	dbDSN := config.GetDatabaseURI()
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
 	if dbDSN != "" {
 		dbrepo.New()
+		router.Get("/ping", handlers.Ping)
 	} else {
 		err := memrepo.New()
 		if err != nil {
 			log.Println("Error initialization memrepo")
 			panic(err)
 		}
+		router.With(mware.RequestHandle, mware.GzipHandle).Post("/", handlers.PostURL)
+		router.With(mware.GzipHandle).Get("/{short}", handlers.GetShort)
+		router.With(mware.RequestHandle, mware.GzipHandle).Post("/api/shorten", handlers.APIShorten)
+		router.Get("/api/user/urls", handlers.GetURLS)
 	}
-
-	router := chi.NewRouter()
-
-	router.Use(middleware.Logger)
-
-	router.With(mware.Checkping).Get("/ping", handlers.Ping)
-	router.With(mware.RequestHandle, mware.GzipHandle).Post("/", handlers.PostURL)
-	router.With(mware.GzipHandle).Get("/{short}", handlers.GetShort)
-	router.With(mware.RequestHandle, mware.GzipHandle).Post("/api/shorten", handlers.APIShorten)
-	router.Get("/api/user/urls", handlers.GetURLS)
 
 	err := http.ListenAndServe(serverAddress, router)
 	if err != nil {
