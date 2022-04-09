d851a50883d436a5087fc81a51a0759d20f63044
lekan <lekan.lekan@gmail.com>
2022-04-09 16:59:58 +0300

    sprint 9 v.1.4

--- internal/storage/newconnector.go
+++ internal/storage/newconnector.go
@@ -6,14 +6,13 @@ import (
 	"github.com/lekan-pvp/short/internal/handlers"
 	"github.com/lekan-pvp/short/internal/storage/dbrepo"
 	"github.com/lekan-pvp/short/internal/storage/memrepo"
-	"strings"
 )
 
 func NewConnector(cfg config.Config) handlers.Repo {
 	switch {
-	case strings.Contains(cfg.FileStoragePath, "storage.json"):
+	case cfg.DatabaseDSN == "":
 		return memrepo.New(cfg.FileStoragePath)
-	case strings.Contains(cfg.DatabaseDSN, "postgresql"):
+	case cfg.FileStoragePath == "":
 		return dbrepo.New(cfg.DatabaseDSN)
 	default:
 		fmt.Printf("unknown repo %s or %s", cfg.DatabaseDSN, cfg.FileStoragePath)
