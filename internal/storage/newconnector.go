24332b2d38b32a869e1cd733ccb8056f3c2dcddd
lekan <lekan.lekan@gmail.com>
2022-04-09 17:16:51 +0300

    sprint 9 v.1.6

--- internal/storage/newconnector.go
+++ internal/storage/newconnector.go
@@ -12,7 +12,7 @@ func NewConnector(cfg config.Config) handlers.Repo {
 	switch {
 	case cfg.DatabaseDSN == "":
 		return memrepo.New(cfg.FileStoragePath)
-	case cfg.FileStoragePath == "":
+	case cfg.FileStoragePath == "storage.json":
 		return dbrepo.New(cfg.DatabaseDSN)
 	default:
 		fmt.Printf("unknown repo %s or %s", cfg.DatabaseDSN, cfg.FileStoragePath)
